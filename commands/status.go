5efef7b665daadb151bbae581cdc1d3d44305b8d
smiley <daniel.account@gmail.com>
2024-01-24 22:12:09 +0000

    cleanup

--- commands/status.go
+++ commands/status.go
@@ -38,17 +38,45 @@ func Status(args []string) {
 		os.Exit(1)
 	}
 
-	fmt.Printf("name: %s\n", task.Name)
-	fmt.Printf("desc: %s\n", task.Description)
-	fmt.Printf("cat: %s\n", task.Category)
+	fmt.Printf("%s (%s) ~ %v(%v)\n", task.Name, task.Category,
+		task.Timestamp, time.Now().UTC().Sub(task.Timestamp))
 
-	fmt.Printf("start: %v, elapsed: %v\n", task.Timestamp, time.Now().UTC().Sub(task.Timestamp))
+	if len(task.Description) > 0 {
+		fmt.Printf("desc: %s\n", task.Description)
+	}
 
 	if len(task.Ip) > 0 {
-		fmt.Printf("remote: %s:%v\n", task.Ip, task.Port)
+		fmt.Printf("    remote: %s:%v\n", task.Ip, task.Port)
 	}
 
 	if len(task.Url) > 0 {
-		fmt.Printf("url: %s\n", task.Url)
+		fmt.Printf("    url: %s\n", task.Url)
+	}
+
+	jsonData, err = os.ReadFile(config.RctfFilesName)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	fmt.Println("files:")
+
+	var files data.Files
+
+	err = json.Unmarshal(jsonData, &files)
+	if err != nil {
+		fmt.Println("error unmarshalling json:", err)
+		os.Exit(1)
+	}
+
+	for idx, file := range files.Files {
+		fmt.Printf("[%v] %s (%v bytes)\n", idx, file.Filename, file.Size)
+		fmt.Printf("     type:   %s...\n", file.Type[:25])
+		fmt.Printf("     md5:    %s\n", file.MD5)
+		fmt.Printf("     sha1:   %s\n", file.SHA1)
+		fmt.Printf("     sha256: %s\n", file.SHA256)
+		if len(file.Comment) > 0 {
+			fmt.Printf("     comment: %s\n", file.Comment)
+		}
+		fmt.Printf("%v(%v)\n", file.Timestamp, time.Now().UTC().Sub(file.Timestamp))
 	}
 }
