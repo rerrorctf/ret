4b13e84dbad703f3ef056017937a3573b0052bbd
smiley <daniel.account@gmail.com>
2024-01-25 10:25:25 +0000

    supply a user config

--- config/config.go
+++ config/config.go
@@ -1,15 +1,16 @@
 package config
 
 const (
-	FolderName        = ".rctf"
-	TaskName          = FolderName + "/task.json"
-	FilesFolderName   = FolderName + "/files"
-	RctfFilesName     = FilesFolderName + "/rctf-files.json"
-	GhidraInstallPath = "/opt/ghidra_11.0_PUBLIC/"
-	GhidraProjectPath = "ghidra"
-	PwnScriptName     = "go.py"
+	UserConfig      = ".config/rctf"
+	FolderName      = ".rctf"
+	TaskName        = FolderName + "/task.json"
+	FilesFolderName = FolderName + "/files"
+	RctfFilesName   = FilesFolderName + "/rctf-files.json"
 )
 
 var (
-	Verbose = false
+	GhidraInstallPath = "/opt/ghidra_11.0_PUBLIC/"
+	GhidraProjectPath = "ghidra"
+	PwnScriptName     = "go.py"
+	Verbose           = false
 )
