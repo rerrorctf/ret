a4417a0ef92665ecee58770c1bd3a5679e9ea7fe
smiley <daniel.account@gmail.com>
2024-01-26 10:50:29 +0000

    bunch of random colour changes

--- commands/monitor.go
+++ commands/monitor.go
@@ -3,13 +3,14 @@ package commands
 import (
 	"fmt"
 	"os"
+	"rctf/theme"
 )
 
 func Monitor(args []string) {
 	if len(args) > 0 {
 		switch args[0] {
 		case "help":
-			fmt.Fprintf(os.Stderr, "usage: rctf monitor\n")
+			fmt.Fprintf(os.Stderr, theme.ColorGreen+"usage"+theme.ColorReset+": rctf "+theme.ColorBlue+"monitor"+theme.ColorReset+"\n")
 			fmt.Fprintf(os.Stderr, "  📡 watch infra for up/down state changes with rctf\n")
 
 			os.Exit(0)
