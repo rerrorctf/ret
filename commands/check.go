c2f12c306c16a8ba7c7d148677ea15ea26c8f8c7
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-01-28 10:50:41 +0000

    add basic setup checker

--- commands/check.go
+++ commands/check.go
@@ -3,8 +3,21 @@ package commands
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"os/user"
+	"rctf/config"
 	"rctf/theme"
 )
+
+func testCommand(command string, args ...string) {
+	c := exec.Command(command, args...)
+	err := c.Run()
+	if err != nil {
+		fmt.Printf("  😔 "+theme.ColorRed+"%v %v\n     -> %v"+theme.ColorReset+"\n", command, args, err)
+	} else {
+		fmt.Printf("  ✅ "+theme.ColorBlue+"%v "+theme.ColorGray+"%v"+theme.ColorReset+"\n", command, args)
+	}
+}
 
 func Check(args []string) {
 	if len(args) > 0 {
@@ -18,4 +31,34 @@ func Check(args []string) {
 	}
 
 	// TODO
+
+	currentUser, err := user.Current()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// system utils
+	testCommand("gcc", "--version")
+	testCommand("clang", "--version")
+	testCommand("strace", "--version")
+	testCommand("gdb", "--version")
+	testCommand("code", "--version")
+	testCommand("subl", "--version")
+
+	// python setup
+	testCommand("python3", "--version")
+	testCommand("pip", "show", "pwntools")
+
+	// re related stuff
+	testCommand("stat", config.GhidraInstallPath+"/ghidraRun")
+	testCommand("stat", config.IdaInstallPath+"/idaRun")
+
+	// pwn related stuff
+	testCommand("stat", currentUser.HomeDir+"/pwndbg/setup.sh")
+	testCommand("one_gadget", "-h")
+
+	// web related stuff
+	testCommand("stat", currentUser.HomeDir+"/BurpSuiteCommunity/BurpSuiteCommunity")
+	testCommand("stat", "/opt/SecLists")
 }
