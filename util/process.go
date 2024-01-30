a28d3d4148dd5f6fe09b1168ba1028277b5757c9
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-01-30 16:52:12 +0000

    when processing files log to a file in the per file directory too

--- util/process.go
+++ util/process.go
@@ -10,7 +10,7 @@ import (
 	"rctf/theme"
 )
 
-func grep2Win(file *data.File, path string) {
+func grep2Win(file *data.File, path string, log *os.File) {
 	jsonData, err := os.ReadFile(config.TaskName)
 	if err != nil {
 		fmt.Println("error reading:", err)
@@ -29,18 +29,28 @@ func grep2Win(file *data.File, path string) {
 	grep2winOutput, err := grep2win.Output()
 	if err == nil {
 		fmt.Printf(theme.ColorPurple+"[grep2win]"+theme.ColorReset+": %s", grep2winOutput)
+		fmt.Fprintf(log, "[grep2win]: %s", grep2winOutput)
 	}
 }
 
-func checksec(file *data.File, path string) {
+func checksec(file *data.File, path string, log *os.File) {
 	checksec := exec.Command("pwn", "checksec", path)
 	checksecOutput, err := checksec.CombinedOutput()
 	if err == nil {
 		fmt.Printf(theme.ColorPurple+"[checksec]"+theme.ColorReset+": %s", checksecOutput)
+		fmt.Fprintf(log, "[checksec]: %s", checksecOutput)
 	}
 }
 
 func ProcessFile(file *data.File, path string) {
-	grep2Win(file, path)
-	checksec(file, path)
+	logFilePath := config.FilesFolderName + "/" + file.SHA256 + "/rctflog.txt"
+	logFile, err := os.Create(logFilePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: unable to open process log %v\n", logFilePath)
+		return
+	}
+	defer logFile.Close()
+
+	grep2Win(file, path, logFile)
+	checksec(file, path, logFile)
 }
