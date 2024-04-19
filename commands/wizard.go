ee3b6bf7a9b1022a84c75b474c127935a57c6cd6
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-04-19 12:12:09 +0100

    wizard should remind the player what the current flag format is
    fixes: https://github.com/rerrorctf/rctf/issues/57

--- commands/wizard.go
+++ commands/wizard.go
@@ -91,6 +91,9 @@ func Wizard(args []string) {
 		interestingFiles = findInterestingFiles()
 	}
 
+	// current flag format reminder
+	fmt.Printf("🧙💬 "+theme.ColorGreen+"The current flag format is "+theme.ColorPurple+"%s\n"+theme.ColorReset, config.FlagFormat)
+
 	// add files
 	if len(interestingFiles) > 0 {
 		if len(interestingFiles) > 1 {
