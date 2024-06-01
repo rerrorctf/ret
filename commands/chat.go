33783b28b2b46af677336bedd52646710dae2bd9
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-06-01 09:35:54 +0100

    improve chat

--- commands/chat.go
+++ commands/chat.go
@@ -14,43 +14,7 @@ import (
 	"time"
 )
 
-func sendToChat(line string) {
-	embed := map[string]interface{}{
-		"title": "ret chat",
-		//"description": "This is an example of a rich embed.",
-		"url":       "https://github.com/rerrorctf/ret/blob/main/commands/chat.go",
-		"color":     rand.Intn(0xFFFFFF),
-		"timestamp": time.Now().UTC().Format(time.RFC3339),
-		//"footer": map[string]string{
-		//"text":     "Footer text",
-		//"icon_url": "https://example.com/footer-icon.png",
-		//},
-		//"thumbnail": map[string]string{
-		//"url": "https://example.com/thumbnail.png",
-		//},
-		//"image": map[string]string{
-		//"url": "https://example.com/image.png",
-		//},
-		//"author": map[string]string{
-		//"name":     "Author Name",
-		//"url":      "https://example.com",
-		//"icon_url": "https://example.com/author-icon.png",
-		//},
-		"fields": []map[string]interface{}{
-			{
-				"name":   "📢",
-				"value":  "`" + line + "`",
-				"inline": false,
-			},
-		},
-	}
-
-	message := map[string]interface{}{
-		"username": config.ChatUsername,
-		//"content":  "Hello, here is a message with an embed:",
-		"embeds": []interface{}{embed},
-	}
-
+func sendMessage(message map[string]interface{}) {
 	body, err := json.Marshal(message)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "💥 "+theme.ColorRed+" error"+theme.ColorReset+": %v\n", err)
@@ -74,9 +38,58 @@ func sendToChat(line string) {
 	resp.Body.Close()
 }
 
+func sendChat(line string) {
+	message := map[string]interface{}{
+		"username": config.ChatUsername,
+		"content":  line,
+	}
+
+	sendMessage(message)
+}
+
+func sendEmbed(line string) {
+	embed := map[string]interface{}{
+		//"title": "ret chat",
+		//"description": "This is an example of a rich embed.",
+		"url":       "https://github.com/rerrorctf/ret/blob/main/commands/chat.go",
+		"color":     rand.Intn(0xFFFFFF),
+		"timestamp": time.Now().UTC().Format(time.RFC3339),
+		//"footer": map[string]string{
+		//"text":     "https://github.com/rerrorctf/ret/blob/main/commands/chat.go",
+		//"icon_url": "https://example.com/footer-icon.png",
+		//},
+		//"thumbnail": map[string]string{
+		//"url": "https://example.com/thumbnail.png",
+		//},
+		//"image": map[string]string{
+		//"url": "https://example.com/image.png",
+		//},
+		//"author": map[string]string{
+		//"name":     "Author Name",
+		//"url":      "https://example.com",
+		//"icon_url": "https://example.com/author-icon.png",
+		//},
+		"fields": []map[string]interface{}{
+			{
+				"name":   "ret chat 📢",
+				"value":  line,
+				"inline": false,
+			},
+		},
+	}
+
+	message := map[string]interface{}{
+		"username": config.ChatUsername,
+		"embeds":   []interface{}{embed},
+	}
+
+	sendMessage(message)
+}
+
 func chatHelp() {
 	fmt.Fprintf(os.Stderr, theme.ColorGreen+"usage"+theme.ColorReset+": ret "+theme.ColorBlue+"chat"+theme.ColorGray+" message"+theme.ColorReset+"\n")
 	fmt.Fprintf(os.Stderr, "  📢 chat with ret\n")
+	fmt.Fprintf(os.Stderr, "     "+theme.ColorGray+"use - to read from stdin"+theme.ColorReset+"\n")
 	fmt.Fprintf(os.Stderr, "  🔗 "+theme.ColorGray+"https://github.com/rerrorctf/ret/blob/main/commands/chat.go"+theme.ColorReset+"\n")
 	os.Exit(0)
 }
@@ -105,10 +118,10 @@ func Chat(args []string) {
 				fmt.Fprintf(os.Stderr, "💥 "+theme.ColorRed+" error"+theme.ColorReset+": %v\n", err)
 				os.Exit(1)
 			}
-			sendToChat(buffer.String())
+			sendEmbed(buffer.String())
 			return
 		}
 	}
 
-	sendToChat(strings.Join(args, " "))
+	sendChat(strings.Join(args, " "))
 }
