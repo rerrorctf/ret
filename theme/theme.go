9e13c53ec836dfcebc5439820ef7d58af340ccf9
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-06-10 22:00:47 +0100

    add underline theme escapes

--- theme/theme.go
+++ theme/theme.go
@@ -1,12 +1,14 @@
 package theme
 
 const (
-	ColorReset  = "\033[0m"
-	ColorRed    = "\033[31m"
-	ColorGreen  = "\033[32m"
-	ColorYellow = "\033[33m"
-	ColorBlue   = "\033[34m"
-	ColorPurple = "\033[35m"
-	ColorCyan   = "\033[36m"
-	ColorGray   = "\033[90m"
+	ColorReset     = "\033[0m"
+	ColorRed       = "\033[31m"
+	ColorGreen     = "\033[32m"
+	ColorYellow    = "\033[33m"
+	ColorBlue      = "\033[34m"
+	ColorPurple    = "\033[35m"
+	ColorCyan      = "\033[36m"
+	ColorGray      = "\033[90m"
+	StartUnderline = "\033[4m"
+	StopUnderline  = "\033[24m"
 )
