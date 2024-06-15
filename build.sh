5251112f1ceaf819929037b2e43a70643b9e149b
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-06-15 21:52:57 +0100

    static build once more

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-go build -buildmode=pie -ldflags "-s -w -X 'main.COMMIT=`git rev-parse --short HEAD`'" -gcflags "all=-trimpath=$GOPATH"
+CGO_ENABLED=0 go build -buildmode=pie -ldflags "-s -w -X 'main.COMMIT=`git rev-parse --short HEAD`' -extldflags '-static'" -gcflags "all=-trimpath=$GOPATH"
