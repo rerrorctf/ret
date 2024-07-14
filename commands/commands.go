3d0e24e58b5959b4da6561a579e245262707de6b
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-07-14 10:14:13 +0100

    move trie to commands.go use trie to to match commands

--- commands/commands.go
+++ commands/commands.go
@@ -1,9 +1,5 @@
 package commands
 
-import (
-	"ret/util"
-)
-
 type CommandFunc func([]string)
 type CommandHelpFunc func()
 
@@ -22,15 +18,67 @@ type Command struct {
 	Arguments []Argument
 }
 
+type CommandTrieNode struct {
+	children map[rune]*CommandTrieNode
+	isEnd    bool
+	command  *Command
+}
+
+type CommandTrie struct {
+	root *CommandTrieNode
+}
+
+func NewTrie() *CommandTrie {
+	return &CommandTrie{root: &CommandTrieNode{children: make(map[rune]*CommandTrieNode)}}
+}
+
+func (t *CommandTrie) Insert(word string, command *Command) {
+	node := t.root
+	for _, char := range word {
+		if _, ok := node.children[char]; !ok {
+			node.children[char] = &CommandTrieNode{children: make(map[rune]*CommandTrieNode)}
+		}
+		node = node.children[char]
+	}
+	node.isEnd = true
+	node.command = command
+}
+
+func (t *CommandTrie) Search(word string) (bool, *Command) {
+	node := t.root
+	for _, char := range word {
+		if _, ok := node.children[char]; !ok {
+			return false, nil
+		}
+		node = node.children[char]
+	}
+	return node.isEnd, node.command
+}
+
+func (t *CommandTrie) ShortestPrefix(word string) (string, string) {
+	prefixLen := 1
+	node := t.root
+	for _, char := range word {
+		if _, ok := node.children[char]; !ok {
+			return "", ""
+		}
+		if len(node.children[char].children) > 1 {
+			prefixLen += 1
+		}
+		node = node.children[char]
+	}
+	return word[:prefixLen], word[prefixLen:]
+}
+
 var (
 	Commands     []Command
-	CommandsTrie *util.Trie
+	CommandsTrie *CommandTrie
 )
 
 func PrepareCommands() {
-	CommandsTrie = util.NewTrie()
+	CommandsTrie = NewTrie()
 
 	for _, command := range Commands {
-		CommandsTrie.Insert(command.Name)
+		CommandsTrie.Insert(command.Name, &command)
 	}
 }
--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strings"
 
 	"ret/commands"
 	"ret/config"
 	"ret/theme"
-	"ret/util"
 )
 
 var (
@@ -41,176 +39,12 @@ func main() {
 
 	command := flag.Arg(0)
 
-	// format
-	if command[0] == 'f' {
-		commands.Format(flag.Args()[1:])
+	found, cmd := commands.CommandsTrie.Search(command)
+
+	if !found {
+		flag.Usage()
 		return
 	}
 
-	// pwn
-	if command[0] == 'p' {
-		if len(command) > 1 {
-			if command[1] == 'w' {
-				commands.Pwn(flag.Args()[1:])
-				return
-			}
-
-			if command[1] == 'r' {
-				commands.Proxy(flag.Args()[1:])
-				return
-			}
-		}
-	}
-
-	// docker
-	if command[0] == 'd' {
-		if len(command) > 1 {
-			if command[1] == 'o' {
-				commands.Docker(flag.Args()[1:])
-				return
-			}
-
-			if command[1] == 'e' {
-				commands.Decompress(flag.Args()[1:])
-				return
-			}
-		}
-	}
-
-	// ghidra, gist
-	if command[0] == 'g' {
-		if len(command) > 1 {
-			if command[1] == 'h' {
-				util.EnsureSkeleton()
-				commands.Ghidra(flag.Args()[1:])
-				return
-			}
-
-			if command[1] == 'i' {
-				commands.Gist(flag.Args()[1:])
-				return
-			}
-
-			if command[1] == 'p' {
-				commands.Gpt(flag.Args()[1:])
-				return
-			}
-		}
-	}
-
-	// ida, inscount
-	if command[0] == 'i' {
-		if len(command) > 1 {
-			if command[1] == 'd' {
-				util.EnsureSkeleton()
-				commands.Ida(flag.Args()[1:])
-				return
-			}
-
-			if command[1] == 'n' {
-				commands.Inscount(flag.Args()[1:])
-				return
-			}
-		}
-	}
-
-	// add, abi
-	if command[0] == 'a' && len(command) > 1 {
-		if command[1] == 'd' {
-			util.EnsureSkeleton()
-			commands.Add(flag.Args()[1:])
-			return
-		}
-
-		if command[1] == 'b' {
-			commands.Abi(flag.Args()[1:])
-			return
-		}
-
-		if command[1] == 'n' {
-			commands.Angr(flag.Args()[1:])
-			return
-		}
-	}
-
-	// status, syscall, sage
-	if command[0] == 's' && len(command) > 1 {
-		if command[1] == 't' {
-			commands.Status(flag.Args()[1:])
-			return
-		}
-
-		if command[1] == 'y' {
-			commands.Syscall(flag.Args()[1:])
-			return
-		}
-
-		if command[1] == 'a' {
-			commands.Sage(flag.Args()[1:])
-			return
-		}
-	}
-
-	// ctf, chat, chef, crypto
-	if command[0] == 'c' {
-		if len(command) > 1 {
-			if command[1] == 't' {
-				commands.Ctf(flag.Args()[1:])
-				return
-			}
-
-			if command[1] == 'h' {
-				if len(command) > 2 {
-					if strings.Compare("cha", command[:3]) == 0 {
-						commands.Chat(flag.Args()[1:])
-						return
-					}
-
-					if strings.Compare("che", command[:3]) == 0 {
-						commands.Chef(flag.Args()[1:])
-						return
-					}
-				}
-			}
-
-			if command[1] == 'r' {
-				commands.Crypto(flag.Args()[1:])
-				return
-			}
-		}
-	}
-
-	// writeup, wizard
-	if command[0] == 'w' && len(command) > 1 {
-		if command[1] == 'r' {
-			commands.Writeup(flag.Args()[1:])
-			return
-		}
-
-		if command[1] == 'i' {
-			commands.Wizard(flag.Args()[1:])
-			return
-		}
-	}
-
-	// libc
-	if command[0] == 'l' {
-		commands.Libc(flag.Args()[1:])
-		return
-	}
-
-	// notes
-	if command[0] == 'n' {
-		commands.Notes(flag.Args()[1:])
-		return
-	}
-
-	// vps
-	if command[0] == 'v' {
-		commands.Vps(flag.Args()[1:])
-		return
-	}
-
-	// help
-	flag.Usage()
+	cmd.Func(flag.Args()[1:])
 }
