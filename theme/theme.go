55539b460189822ee70619b86120f7686f13679e
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-07-14 23:06:26 +0100

    move remove colors to theme.go

--- theme/theme.go
+++ theme/theme.go
@@ -1,5 +1,7 @@
 package theme
 
+import "strings"
+
 const (
 	ColorReset     = "\033[0m"
 	ColorRed       = "\033[31m"
@@ -12,3 +14,17 @@ const (
 	StartUnderline = "\033[4m"
 	StopUnderline  = "\033[24m"
 )
+
+func RemoveColors(message string) string {
+	message = strings.ReplaceAll(message, ColorReset, "")
+	message = strings.ReplaceAll(message, ColorRed, "")
+	message = strings.ReplaceAll(message, ColorGreen, "")
+	message = strings.ReplaceAll(message, ColorYellow, "")
+	message = strings.ReplaceAll(message, ColorBlue, "")
+	message = strings.ReplaceAll(message, ColorPurple, "")
+	message = strings.ReplaceAll(message, ColorCyan, "")
+	message = strings.ReplaceAll(message, ColorGray, "")
+	message = strings.ReplaceAll(message, StartUnderline, "")
+	message = strings.ReplaceAll(message, StopUnderline, "")
+	return message
+}
