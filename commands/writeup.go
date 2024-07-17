fb8d76dafb28d234b6c38bac69e73cf96b33413e
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-07-18 00:25:23 +0100

    use the stashed ctftime url when generating the writeup
    fixes: https://github.com/rerrorctf/ret/issues/160

--- commands/writeup.go
+++ commands/writeup.go
@@ -20,18 +20,19 @@ func init() {
 		Help:      WriteupHelp,
 		Url:       "https://github.com/rerrorctf/ret/blob/main/commands/wizard.go",
 		Arguments: nil,
-		SeeAlso:   []string{"notes", "pwn", "capture", "format"}})
+		SeeAlso:   []string{"notes", "pwn", "capture", "format", "ctftime"}})
 }
 
 func WriteupHelp() string {
 	return "create a markdown writeup using a template with ret\n\n" +
 		"the writeup will saved in a file called `writeup.md`\n\n" +
 		"if a file called `writeup.md` already exists the command will abort\n\n" +
-		"1. imports all notes taken with the " + theme.ColorGreen + "`notes`" + theme.ColorReset + " command into the description area\n" +
-		"2. creates a space for a python script and then imports the script created by " + theme.ColorGreen + "`pwn`" + theme.ColorReset + " if one exists\n" +
-		"3. imports the flag captured with the " + theme.ColorGreen + "`capture`" + theme.ColorReset + " command if one exists or the regex specfied with " + theme.ColorGreen + "`format`" + theme.ColorReset + " if one does not\n" +
-		"4. uses the " + theme.ColorYellow + "`\"username\"`" + theme.ColorReset + " from " + theme.ColorCyan + "`~/.config/ret`" + theme.ColorReset + " to attribute to this writeup to you\n" +
-		"5. inserts a date stamp for today's date using yyyy/mm/dd format\n"
+		"1. uses the " + theme.ColorYellow + "`\"ctftimeurl\"`" + theme.ColorReset + " to insert a url at the top of the writeup\n" +
+		"2. imports all notes taken with the " + theme.ColorGreen + "`notes`" + theme.ColorReset + " command into the description area\n" +
+		"3. creates a space for a python script and then imports the script created by " + theme.ColorGreen + "`pwn`" + theme.ColorReset + " if one exists\n" +
+		"4. imports the flag captured with the " + theme.ColorGreen + "`capture`" + theme.ColorReset + " command if one exists or the regex specfied with " + theme.ColorGreen + "`format`" + theme.ColorReset + " if one does not\n" +
+		"5. uses the " + theme.ColorYellow + "`\"username\"`" + theme.ColorReset + " from " + theme.ColorCyan + "`~/.config/ret`" + theme.ColorReset + " to attribute to this writeup to you\n" +
+		"6. inserts a date stamp for today's date using yyyy/mm/dd format\n"
 }
 
 func Writeup(args []string) {
@@ -41,6 +42,11 @@ func Writeup(args []string) {
 
 	if !os.IsNotExist(err) {
 		log.Fatalf("💥 "+theme.ColorRed+"error"+theme.ColorReset+": \"%s\" already exists!\n", filePath)
+	}
+
+	url := config.CtfTimeUrl
+	if url == "" {
+		url = "https://ctftime.link.goes.here"
 	}
 
 	flag, err := util.GetCurrentFlag()
@@ -72,7 +78,7 @@ func Writeup(args []string) {
 	date := time.Now().Format("2006/01/02")
 
 	template := fmt.Sprintf(
-		"https://chal.link.goes.here\n\n"+
+		"%s\n\n"+
 			"# TASK-NAME (CATEGORY)\n\n"+
 			"DESCRIPTION-GOES-HERE\n\n"+
 			"%s"+
@@ -81,7 +87,7 @@ func Writeup(args []string) {
 			"%s"+
 			"```\n\n"+
 			"## Flag\n`%s`\n\n"+
-			"%s %s\n", notesStr, script, flag, name, date)
+			"%s %s\n", url, notesStr, script, flag, name, date)
 
 	err = os.WriteFile(filePath, []byte(template), 0644)
 
