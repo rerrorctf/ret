120b4a9e999551195921319d9e54e2e55a058ced
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-07-19 14:14:24 +0100

    switch the gpt command to use util.Gpt

--- commands/gpt.go
+++ commands/gpt.go
@@ -2,14 +2,13 @@ package commands
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
 	"log"
-	"net/http"
 	"os"
 	"ret/config"
 	"ret/theme"
+	"ret/util"
 	"strings"
 	"time"
 )
@@ -41,7 +40,7 @@ func GptHelp() string {
 }
 
 const (
-	GPT_PROMPT string = `You are a CTF assistant specializing in various categories:
+	GPT_COMMAND_PROMPT string = `You are a CTF assistant specializing in various categories:
 
 	- **Cryptography**: Decrypting messages, encryption algorithms, cryptographic puzzles.
 	- **Steganography**: Detecting hidden information in media files.
@@ -79,51 +78,6 @@ func gptSpinner(stop chan bool) {
 	}
 }
 
-func sendRequest(query map[string]interface{}) string {
-	body, err := json.Marshal(query)
-	if err != nil {
-		log.Fatalf("💥 "+theme.ColorRed+" error"+theme.ColorReset+": %v\n", err)
-	}
-
-	req, err := http.NewRequest("POST", "https://api.openai.com/v1/chat/completions", bytes.NewBuffer(body))
-	if err != nil {
-		log.Fatalf("💥 "+theme.ColorRed+" error"+theme.ColorReset+": %v\n", err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", config.OpenAIKey))
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("💥 "+theme.ColorRed+" error"+theme.ColorReset+": %v\n", err)
-	}
-	defer resp.Body.Close()
-
-	body, err = io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("💥 "+theme.ColorRed+" error"+theme.ColorReset+": %v\n", err)
-	}
-
-	var result map[string]interface{}
-	if err := json.Unmarshal(body, &result); err != nil {
-		log.Fatalf("💥 "+theme.ColorRed+" error"+theme.ColorReset+": %v\n", err)
-	}
-
-	if choices, ok := result["choices"].([]interface{}); ok {
-		if len(choices) > 0 {
-			if choice, ok := choices[0].(map[string]interface{}); ok {
-				if message, ok := choice["message"].(map[string]interface{}); ok {
-					if content, ok := message["content"].(string); ok {
-						return content
-					}
-				}
-			}
-		}
-	}
-
-	return ""
-}
-
 func readInput(args []string) string {
 	content := strings.Join(args[0:], " ")
 
@@ -151,28 +105,13 @@ func Gpt(args []string) {
 
 	content := readInput(args)
 
-	query := map[string]interface{}{
-		"model": config.OpenAIModel,
-		"messages": []map[string]interface{}{
-			{
-				"role":    "system",
-				"content": GPT_PROMPT,
-			},
-			{
-				"role":    "user",
-				"content": content,
-			},
-		},
-		"temperature": 0.3,
-	}
-
 	fmt.Printf("🧠 " + theme.ColorGray + config.OpenAIModel + theme.ColorReset + "\n")
 
 	stop := make(chan bool)
 
 	go gptSpinner(stop)
 
-	answer := sendRequest(query)
+	answer := util.Gpt(GPT_COMMAND_PROMPT, content)
 
 	stop <- true
 
