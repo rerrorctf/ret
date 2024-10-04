e939715171e0fa71a206426a1f9258c8b321bc62
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-10-04 13:33:20 +0100

    add mach-o magic to file types

--- data/files.go
+++ data/files.go
@@ -2,11 +2,13 @@ package data
 
 const (
 	FILE_TYPE_ELF     string = "elf"
+	FILE_TYPE_MACHO   string = "mach-o"
 	FILE_TYPE_UNKNOWN string = "unknown"
 )
 
 var FileMagics = map[string][]byte{
-	FILE_TYPE_ELF: {0x7F, 0x45, 0x4C, 0x46},
+	FILE_TYPE_ELF:   {0x7F, 0x45, 0x4C, 0x46},
+	FILE_TYPE_MACHO: {0xCA, 0xFE, 0xBA, 0xBE},
 }
 
 type File struct {
