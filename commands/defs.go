304bd3aa0c614e190b5c703d14b0241fb10b7635
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-12-13 10:34:20 +0000

    add std file nos

--- commands/defs.go
+++ commands/defs.go
@@ -27,6 +27,19 @@ func printColoredDef(path string, def string, val string) {
 	fmt.Printf(theme.ColorPurple + "#define " + theme.ColorBlue + def + " " + theme.ColorReset + val + "\n")
 }
 
+func printStdFileNos() {
+	printColoredDefTitle("STDIN_FILENO")
+	printColoredDef("unistd.h", "STDIN_FILENO", "0")
+
+	printColoredDefTitle("STDOUT_FILENO")
+	printColoredDef("unistd.h", "STDOUT_FILENO", "1")
+
+	printColoredDefTitle("STDERR_FILENO")
+	printColoredDef("unistd.h", "STDERR_FILENO", "2")
+
+	fmt.Println()
+}
+
 func printSeeks() {
 	printColoredDefTitle("SEEK_SET")
 	printColoredDef("/usr/include/linux/fs.h", "SEEK_SET", "0")
@@ -39,9 +52,14 @@ func printSeeks() {
 	printColoredDefTitle("SEEK_END")
 	printColoredDef("/usr/include/linux/fs.h", "SEEK_END", "2")
 	printColoredDef("/usr/include/stdio.h", "SEEK_END", "2")
+
+	fmt.Println()
 }
 
 func Defs(args []string) {
+	// STDIN_FILENO / STDOUT_FILENO / STDERR_FILENO
+	printStdFileNos()
+
 	// SEEK_SET / SEEK_CUR / SEEK_END
 	printSeeks()
 }
