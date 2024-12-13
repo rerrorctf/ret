7cb74a32b3583ffb6af291506efe0ec027a794d8
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-12-13 10:43:29 +0000

    add an include to the title, add some memory related defs

--- commands/defs.go
+++ commands/defs.go
@@ -18,42 +18,59 @@ func DefsHelp() string {
 	return ""
 }
 
-func printColoredDefTitle(def string) {
-	fmt.Printf(theme.ColorGreen + def + "\n" + theme.ColorReset)
+func printColoredDefTitle(def string, include string) {
+	if len(include) > 0 {
+		fmt.Printf(theme.ColorGreen + def + theme.ColorGray + " #include <" + include + ">\n" + theme.ColorReset)
+	} else {
+		fmt.Printf(theme.ColorGreen + def + "\n" + theme.ColorReset)
+	}
 }
 
 func printColoredDef(path string, def string, val string) {
-	fmt.Printf(theme.ColorGray + "  " + path + " ~ " + theme.ColorReset)
+	if len(path) > 0 {
+		fmt.Printf(theme.ColorGray + "  " + path + " ~ " + theme.ColorReset)
+	}
 	fmt.Printf(theme.ColorPurple + "#define " + theme.ColorBlue + def + " " + theme.ColorReset + val + "\n")
 }
 
 func printStdFileNos() {
-	printColoredDefTitle("STDIN_FILENO")
-	printColoredDef("unistd.h", "STDIN_FILENO", "0")
+	printColoredDefTitle("STDIN_FILENO", "unistd.h")
+	printColoredDef("", "STDIN_FILENO", "0")
 
-	printColoredDefTitle("STDOUT_FILENO")
-	printColoredDef("unistd.h", "STDOUT_FILENO", "1")
+	printColoredDefTitle("STDOUT_FILENO", "unistd.h")
+	printColoredDef("", "STDOUT_FILENO", "1")
 
-	printColoredDefTitle("STDERR_FILENO")
-	printColoredDef("unistd.h", "STDERR_FILENO", "2")
+	printColoredDefTitle("STDERR_FILENO", "unistd.h")
+	printColoredDef("", "STDERR_FILENO", "2")
 
 	fmt.Println()
 }
 
 func printSeeks() {
-	printColoredDefTitle("SEEK_SET")
+	printColoredDefTitle("SEEK_SET", "stdio.h")
 	printColoredDef("/usr/include/linux/fs.h", "SEEK_SET", "0")
 	printColoredDef("/usr/include/stdio.h", "SEEK_SET", "0")
 
-	printColoredDefTitle("SEEK_CUR")
+	printColoredDefTitle("SEEK_CUR", "stdio.h")
 	printColoredDef("/usr/include/linux/fs.h", "SEEK_CUR", "1")
 	printColoredDef("/usr/include/stdio.h", "SEEK_CUR", "1")
 
-	printColoredDefTitle("SEEK_END")
+	printColoredDefTitle("SEEK_END", "stdio.h")
 	printColoredDef("/usr/include/linux/fs.h", "SEEK_END", "2")
 	printColoredDef("/usr/include/stdio.h", "SEEK_END", "2")
 
 	fmt.Println()
+}
+
+func printMem() {
+	printColoredDefTitle("PROT_READ", "sys/mman.h")
+	printColoredDef("/usr/include/asm-generic/mman-common.h", "PROT_READ", "1")
+
+	printColoredDefTitle("PROT_WRITE", "sys/mman.h")
+	printColoredDef("/usr/include/asm-generic/mman-common.h", "PROT_WRITE", "2")
+
+	printColoredDefTitle("PROT_EXEC", "sys/mman.h")
+	printColoredDef("/usr/include/asm-generic/mman-common.h", "PROT_EXEC", "4")
 }
 
 func Defs(args []string) {
@@ -62,4 +79,7 @@ func Defs(args []string) {
 
 	// SEEK_SET / SEEK_CUR / SEEK_END
 	printSeeks()
+
+	// PROT_READ / PROT_WRITE / PROT_EXEC
+	printMem()
 }
