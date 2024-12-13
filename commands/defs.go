50afc25742c2f8016737e3548dc867c5015bf321
dmur1 <93072266+dmur1@users.noreply.github.com>
2024-12-13 10:29:18 +0000

    add two pretty print functions and use them

--- commands/defs.go
+++ commands/defs.go
@@ -18,28 +18,30 @@ func DefsHelp() string {
 	return ""
 }
 
+func printColoredDefTitle(def string) {
+	fmt.Printf(theme.ColorGreen + def + "\n" + theme.ColorReset)
+}
+
+func printColoredDef(path string, def string, val string) {
+	fmt.Printf(theme.ColorGray + "  " + path + " ~ " + theme.ColorReset)
+	fmt.Printf(theme.ColorPurple + "#define " + theme.ColorBlue + def + " " + theme.ColorReset + val + "\n")
+}
+
+func printSeeks() {
+	printColoredDefTitle("SEEK_SET")
+	printColoredDef("/usr/include/linux/fs.h", "SEEK_SET", "0")
+	printColoredDef("/usr/include/stdio.h", "SEEK_SET", "0")
+
+	printColoredDefTitle("SEEK_CUR")
+	printColoredDef("/usr/include/linux/fs.h", "SEEK_CUR", "1")
+	printColoredDef("/usr/include/stdio.h", "SEEK_CUR", "1")
+
+	printColoredDefTitle("SEEK_END")
+	printColoredDef("/usr/include/linux/fs.h", "SEEK_END", "2")
+	printColoredDef("/usr/include/stdio.h", "SEEK_END", "2")
+}
+
 func Defs(args []string) {
-	fmt.Printf(theme.ColorGreen + "SEEK_SET\n" + theme.ColorReset)
-
-	fmt.Printf(theme.ColorGray + "  /usr/include/linux/fs.h ~ " + theme.ColorReset)
-	fmt.Printf(theme.ColorPurple + "#define " + theme.ColorBlue + "SEEK_SET " + theme.ColorReset + "0\n")
-
-	fmt.Printf(theme.ColorGray + "  /usr/include/stdio.h ~ " + theme.ColorReset)
-	fmt.Printf(theme.ColorPurple + "#define " + theme.ColorBlue + "SEEK_SET " + theme.ColorReset + "0\n")
-
-	fmt.Printf(theme.ColorGreen + "SEEK_CUR\n" + theme.ColorReset)
-
-	fmt.Printf(theme.ColorGray + "  /usr/include/linux/fs.h ~ " + theme.ColorReset)
-	fmt.Printf(theme.ColorPurple + "#define " + theme.ColorBlue + "SEEK_CUR " + theme.ColorReset + "1\n")
-
-	fmt.Printf(theme.ColorGray + "  /usr/include/stdio.h ~ " + theme.ColorReset)
-	fmt.Printf(theme.ColorPurple + "#define " + theme.ColorBlue + "SEEK_CUR " + theme.ColorReset + "1\n")
-
-	fmt.Printf(theme.ColorGreen + "SEEK_END\n" + theme.ColorReset)
-
-	fmt.Printf(theme.ColorGray + "  /usr/include/linux/fs.h ~ " + theme.ColorReset)
-	fmt.Printf(theme.ColorPurple + "#define " + theme.ColorBlue + "SEEK_END " + theme.ColorReset + "2\n")
-
-	fmt.Printf(theme.ColorGray + "  /usr/include/stdio.h ~ " + theme.ColorReset)
-	fmt.Printf(theme.ColorPurple + "#define " + theme.ColorBlue + "SEEK_END " + theme.ColorReset + "2\n")
+	// SEEK_SET / SEEK_CUR / SEEK_END
+	printSeeks()
 }
