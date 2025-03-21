8c06b8058dceca01f6151bfcb3396547f1cbfe44
dmur1 <93072266+dmur1@users.noreply.github.com>
2025-03-21 09:49:49 +0000

    Revert "delete the angr command"
    This reverts commit 9ac36dce753113b64f5c23df3936a6bc0e8762e8.

---
+++ commands/angr.go
@@ -0,0 +1,60 @@
+package commands
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"ret/theme"
+)
+
+func init() {
+	Commands = append(Commands, Command{
+		Name:      "angr",
+		Emoji:     "😠",
+		Func:      Angr,
+		Help:      AngrHelp,
+		Arguments: nil})
+}
+
+func AngrHelp() string {
+	return "runs the angr docker with ret\n\n" +
+		"mounts the current working directory as a volume\n\n" +
+		"note that this command requires docker\n\n" +
+		"effectively runs:\n" +
+		"```bash\n" +
+		theme.ColorGray + "$ " + theme.ColorBlue + "sudo docker pull angr/angr\n" +
+		theme.ColorGray + "$ " + theme.ColorBlue + "sudo docker run -it -v $PWD:/home/angr/x angr/angr\n" + theme.ColorReset +
+		"```\n\n" +
+		"see https://docs.angr.io/en/latest/getting-started/installing.html#installing-with-docker for more information\n" + theme.ColorReset
+}
+
+func Angr(args []string) {
+	pull := exec.Command("sudo", "docker", "pull", "angr/angr")
+
+	pull.Stdin = os.Stdin
+	pull.Stdout = os.Stdout
+	pull.Stderr = os.Stderr
+
+	err := pull.Run()
+	if err != nil {
+		log.Fatalf("💥 "+theme.ColorRed+"error"+theme.ColorReset+": %v\n", err)
+	}
+
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("💥 "+theme.ColorRed+"error"+theme.ColorReset+": %v\n", err)
+	}
+
+	run := exec.Command("sudo", "docker", "run", "-it", "-v", fmt.Sprintf("%s:/home/angr/x", dir), "angr/angr")
+
+	run.Stdin = os.Stdin
+	run.Stdout = os.Stdout
+	run.Stderr = os.Stderr
+
+	err = run.Run()
+
+	if err != nil {
+		log.Fatalf("💥 "+theme.ColorRed+"error"+theme.ColorReset+": %v\n", err)
+	}
+}
